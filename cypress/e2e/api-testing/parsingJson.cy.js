0a75caf0c2fc6530eb01d419c209cf0778feb66f
Volodymyr Horbachov <gorbacovvolodimir@gmail.com>
2023-12-23 17:32:24 +0200

    JSON schema validation

--- cypress/e2e/api-testing/parsingJson.cy.js
+++ cypress/e2e/api-testing/parsingJson.cy.js
@@ -1,5 +1,8 @@
 /// <reference types="cypress" />
 
+const Ajv = require('ajv');
+const avj = new Ajv();
+
 describe("Parsing JSON", () => {
 
     it('Simple JSON request parsing', () => {
@@ -27,6 +30,70 @@ describe("Parsing JSON", () => {
             });
             expect(totalPrice).to.eq(899.23)
         })
+    });
+
+    it('JSON schema validation', () => {
+
+        cy.request('GET', 'https://fakestoreapi.com/products?limit=5')
+        .then((response) => {
+
+            const schema = {
+                "$schema": "http://json-schema.org/draft-07/schema#",
+                "title": "Generated schema for Root",
+                "type": "array",
+                "items": {
+                  "type": "object",
+                  "properties": {
+                    "id": {
+                      "type": "number"
+                    },
+                    "title": {
+                      "type": "string"
+                    },
+                    "price": {
+                      "type": "number"
+                    },
+                    "description": {
+                      "type": "string"
+                    },
+                    "category": {
+                      "type": "string"
+                    },
+                    "image": {
+                      "type": "string"
+                    },
+                    "rating": {
+                      "type": "object",
+                      "properties": {
+                        "rate": {
+                          "type": "number"
+                        },
+                        "count": {
+                          "type": "number"
+                        }
+                      },
+                      "required": [
+                        "rate",
+                        "count"
+                      ]
+                    }
+                  },
+                  "required": [
+                    "id",
+                    "title",
+                    "price",
+                    "description",
+                    "category",
+                    "image",
+                    "rating"
+                  ]
+                }
+            } // schema ends
+
+            const validate = avj.compile(schema)
+            const isValid = validate(response.body)
+            expect(isValid).to.be.true
+        })        
     });
 
 })
