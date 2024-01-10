60de1284dae9df9b86fb3114c5e53a45915d4e47
Volodymyr Horbachov <gorbacovvolodimir@gmail.com>
2024-01-10 17:36:04 +0200

    OAuth2  authentication

--- cypress/e2e/api-testing/authentication.cy.js
+++ cypress/e2e/api-testing/authentication.cy.js
@@ -37,7 +37,7 @@ describe("Authentications", () => {
     })
 
     const token = 'ghp_pfEbzmLTTeZZdCzITSusLYur4dpnu022ZxvB'
-    it('Bearer Authentications', () => {
+    it.skip('Bearer Authentications', () => {
         cy.request({
             method: 'GET',
             url: 'https://api.github.com/user/repos',
@@ -48,6 +48,39 @@ describe("Authentications", () => {
         .then((response) => {
 
             expect(response.status).to.eq(200)
+        })
+    })
+    const accesToken = '';
+    it.skip('OAuth2 Authentications get token', () => {
+        cy.request({
+            method: 'POST',
+            url: 'https://github.com/login/oauth/acces_token',
+            qs: {
+                client_id: 'ded8c34b1cbcdcaf7149',
+                client_secret: '1c10f2559255c03d07bf97c8567eae2e913cccda',
+                code: '5a8a3fd47f484494fe6d'
+            }
+        })
+        .then((response) => {
+
+            const params = response.body.split('&');
+            accesToken = params[0].split('=')[1]
+
+        })
+    })
+
+    it.skip('OAuth2 Authentications', () => {
+        cy.request({
+            method: 'GET',
+            url: 'https://api.github.com/user/repos',
+            headers: {
+                Autherization: 'Bearer ' + accesToken
+            }
+        })
+        .then((response) => {
+
+            expect(response.status).to.eq(200)
+
         })
     })
 })
